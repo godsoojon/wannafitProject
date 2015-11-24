d1d42a46ad30f6f16d3b62bb0f906ae269ffd833
hongjun423 <hongjun423@pcname>
2015-11-24 22:02:35 +0900

    회원가입 validate

--- WannaFitShare/src/com/wannafitshare/controller/PartyController.java
+++ WannaFitShare/src/com/wannafitshare/controller/PartyController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.wannafitshare.customer.service.PartyListService;
 import com.wannafitshare.customer.service.PartyService;
 import com.wannafitshare.vo.Customer;
 import com.wannafitshare.vo.Party;
