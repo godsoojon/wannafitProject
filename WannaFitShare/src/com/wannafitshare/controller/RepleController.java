e1a622d3c697056f69b30478b9b1060d85a96881
godsoojon <soojongk@naver.com>
2015-12-02 09:49:47 +0900

    2

--- WannaFitShare/src/com/wannafitshare/controller/RepleController.java
+++ WannaFitShare/src/com/wannafitshare/controller/RepleController.java
@@ -1,5 +1,5 @@
 package com.wannafitshare.controller;
-
+import javax.swing.JOptionPane;
 import java.util.Date;
 import java.util.List;
 
@@ -57,15 +57,16 @@ public class RepleController {
     	String url="";
     	Customer customer =(Customer)session.getAttribute("loginInfo");
     	String id1=customer.getCsId();
-    	PhotoUpload photo=(PhotoUpload)session.getAttribute("photo");
-    	String id2=photo.getCsId();
     	
     	
-    	if(id1.equals(id2)){
+    	
+    	
+    	if(id1.equals(repleId)){
     		url="picture/reple_register.tiles";
     		model.addAttribute("repleId",repleId);
     		
     	}else{
+    		JOptionPane.showMessageDialog(null, "고객님이 쓴 글이 아닙니다.");
     		url="/reple/return.do";
     	}
     	return url;
@@ -77,9 +78,8 @@ public class RepleController {
     	PhotoUpload photo =(PhotoUpload)session.getAttribute("photo");
     	Reple reple=new Reple(repleId,customer.getCsId(),photo.getPhotoId(),repletxt,new Date());
     	
-    	
     	rService.updateReple(reple);
-    	
+    	JOptionPane.showMessageDialog(null, "수정되었습니다.");
     	return "/reple/return.do";
     	
     }
@@ -128,6 +128,7 @@ public class RepleController {
     	Reple reple=rService.findRepleOne(repleId);
     	String id2 =reple.getCsId();
     	if(id1.equals(id2)){
+    		JOptionPane.showMessageDialog(null, "삭제되었습니다.");
     		rService.deleteReple(repleId);
     	}
     	
